fd66e7d8f51a60313fe5485a7ed79d9c6f54347f
kr-chirag <chirag.p@knackroot.com>
2025-04-02 16:03:49 +0530

    fix: spelling

--- contracts/Swaping.sol
+++ contracts/Swaping.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.28;
 
 import "@uniswap/v2-periphery/contracts/interfaces/IUniswapV2Router02.sol";
@@ -7,14 +7,14 @@ import "@openzeppelin/contracts/interfaces/IERC20.sol";
 contract Swaping {
     IUniswapV2Router02 public router =
         IUniswapV2Router02(0xeE567Fe1712Faf6149d80dA1E6934E354124CfE3);
-    address weth = router.WETH();
-    address usdc = 0x1c7D4B196Cb0C7B01d743Fbc6116a902379C7238;
-    address uro = 0x08210F9170F89Ab7658F0B5E3fF39b0E03C594D4;
+    address WETH = router.WETH();
+    address USDC = 0x1c7D4B196Cb0C7B01d743Fbc6116a902379C7238;
+    address EURC = 0x08210F9170F89Ab7658F0B5E3fF39b0E03C594D4;
 
-    address[] pathWthToUSDC = [weth, usdc];
-    address[] pathUSDCToETH = [usdc, weth];
-    address[] pathUROToUSDC = [uro, usdc];
-    address[] pathUSDCToURO = [usdc, uro];
+    address[] pathWthToUSDC = [WETH, USDC];
+    address[] pathUSDCToETH = [USDC, WETH];
+    address[] pathEURCToUSDC = [EURC, USDC];
+    address[] pathUSDCToEURC = [USDC, EURC];
 
     function swapExactETHForTokens() public payable {
         router.swapExactETHForTokens{value: msg.value}(
@@ -25,33 +25,33 @@ contract Swaping {
         );
     }
 
-    function swapUROForUSDC(uint256 amount) public {
-        IERC20(uro).transferFrom(msg.sender, address(this), amount);
-        IERC20(uro).approve(address(router), amount);
+    function swapEURCForUSDC(uint256 amount) public {
+        IERC20(EURC).transferFrom(msg.sender, address(this), amount);
+        IERC20(EURC).approve(address(router), amount);
         router.swapExactTokensForTokens(
             amount,
             0,
-            pathUROToUSDC,
+            pathEURCToUSDC,
             msg.sender,
             block.timestamp + 300
         );
     }
 
-    function swapUSDForURO(uint256 amount) public {
-        IERC20(usdc).transferFrom(msg.sender, address(this), amount);
-        IERC20(usdc).approve(address(router), amount);
+    function swapUSDForEURC(uint256 amount) public {
+        IERC20(USDC).transferFrom(msg.sender, address(this), amount);
+        IERC20(USDC).approve(address(router), amount);
         router.swapExactTokensForTokens(
             amount,
             0,
-            pathUSDCToURO,
+            pathUSDCToEURC,
             msg.sender,
             block.timestamp + 300
         );
     }
 
     function swapUSDForETH(uint256 amount) public {
-        IERC20(usdc).transferFrom(msg.sender, address(this), amount);
-        IERC20(usdc).approve(address(router), amount);
+        IERC20(USDC).transferFrom(msg.sender, address(this), amount);
+        IERC20(USDC).approve(address(router), amount);
         router.swapExactTokensForETH(
             amount,
             0,
